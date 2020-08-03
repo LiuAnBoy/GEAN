964a64f6ed33cf28483ad8a062cd5386512c2295
Eric.LA Chen(陳律安) <eric.la_chen@ericlachen.htctaipei.htc.com.tw>
2020-08-03 17:39:40 +0800

    update

--- component/navbar/navbar.component.jsx
+++ component/navbar/navbar.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Layout, Menu } from 'antd'
 
+import './navbar.styles.scss'
+
 import {
   MailOutlined,
   AppstoreOutlined,
@@ -14,7 +16,7 @@ class Navbar extends React.Component {
     current: 'mail',
   }
 
-  handleClick = e => {
+  handleClick = (e) => {
     console.log('click ', e)
     this.setState({ current: e.key })
   }
@@ -26,41 +28,48 @@ class Navbar extends React.Component {
         <Menu
           onClick={this.handleClick}
           selectedKeys={[current]}
-          mode='horizontal'
+          mode="horizontal"
           style={{
             backgroundColor: '#0F1F2E',
             float: 'right',
             color: 'white',
             height: 60,
-          }}>
+          }}
+        >
           <Menu.Item
-            key='mail'
+            key="mail"
             icon={<MailOutlined />}
-            style={{ margin: 0, width: 200, height: 60, alignItems:'center', textAlign: 'center',textDecoration: 'none' }}>
+            className="Nav-btn"
+            style={{margin: 0}}
+          >
             Navigation One
           </Menu.Item>
           <Menu.Item
-            key='mail'
+            key="mail"
             icon={<MailOutlined />}
-            style={{ margin: 0, width: 200, height: 60, alignItems:'center', textAlign: 'center',textDecoration: 'none' }}>
+            className="Nav-btn"
+          >
             Navigation One
           </Menu.Item>
           <Menu.Item
-            key='mail'
+            key="mail"
             icon={<MailOutlined />}
-            style={{ margin: 0, width: 200, height: 60, alignItems:'center', textAlign: 'center',textDecoration: 'none' }}>
+            className="Nav-btn"
+          >
             Navigation One
           </Menu.Item>
           <Menu.Item
-            key='mail'
+            key="mail"
             icon={<MailOutlined />}
-            style={{ margin: 0, width: 200, height: 60, alignItems:'center', textAlign: 'center',textDecoration: 'none' }}>
+            className="Nav-btn"
+          >
             Navigation One
           </Menu.Item>
           <Menu.Item
-            key='mail'
+            key="mail"
             icon={<MailOutlined />}
-            style={{ margin: 0, width: 200, height: 60, alignItems:'center', textAlign: 'center',textDecoration: 'none' }}>
+            className="Nav-btn"
+          >
             Navigation One
           </Menu.Item>
         </Menu>
